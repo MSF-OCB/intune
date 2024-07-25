d24f87d21aca11953821086b1182decec41b888f
YvesdeVoghel <35533372+YvesdeVoghel@users.noreply.github.com>
2024-07-25 16:59:33 +0200

    Create RemoveQuickAssist.ps1

---
+++ Scripts/Bloatware/RemoveQuickAssist.ps1
@@ -0,0 +1,8 @@
+# Use windows capabilities to remove older QuickAssist
+$WinVer = (Get-CimInstance Win32_OperatingSystem).version
+If ($WinVer -lt 10.0.2){
+      $QuickAssistStat = Get-WindowsCapability -online -Name *QuickAssist*
+      If ($QuickAssistStat.State -eq 'Installed') {
+              Remove-WindowsCapability -Online -Name $QuickAssistStat.name -ErrorAction Continue
+      }else {Write-Host "Quick assist is not installed." -ForegroundColor Cyan}
+} else {Write-Host "`nYou are running Win 11, uninstalltion of Quick Assist will be via AppxPackages.`n" -ForegroundColor Cyan}
