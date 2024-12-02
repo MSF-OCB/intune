8b830cbf9cce76d6d9cd92ac2c28e45e80498d70
omar-ocb <95644130+omar-ocb@users.noreply.github.com>
2024-12-02 11:53:45 +0100

    Update Install_PPKG.cmd

--- Scripts/Enrollment/PPKG/Install_PPKG.cmd
+++ Scripts/Enrollment/PPKG/Install_PPKG.cmd
@@ -3,4 +3,5 @@
 IF NOT EXIST "c:\Temp" (
     mkdir "c:\Temp"
 )
-powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Webrequest 'https://raw.githubusercontent.com/MSF-OCB/intune/refs/heads/main/Scripts/Enrollment/PPKG/PPKG_Fetch.ps1' -OutFile C:\Temp\PPKG_Fetch.ps1; C:\Temp\PPKG_Fetch.ps1
+powershell.exe Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Webrequest 'https://raw.githubusercontent.com/MSF-OCB/intune/refs/heads/main/Scripts/Enrollment/PPKG/PPKG_Fetch.exe' -OutFile C:\Temp\PPKG_Fetch.exe
+start C:\Temp\PPKG_Fetch.exe
